81120ecc1a21ef879365933febee053febfeae02
Luke Salter <luke@churchsuite.com>
2022-10-14 22:19:20 +0100

    Adding a couple of views and removing vite & mix

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn() => view('home'));
+Route::get('/register', fn() => view('register'));
