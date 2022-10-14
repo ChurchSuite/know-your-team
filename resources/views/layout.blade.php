fa3c7c63d11db00cc37cfb86d916bb60b7bd783e
Ant Weedon <ant@churchsuite.com>
2022-10-14 21:47:12 +0100

    Added tailwind

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<title>HTML 5 Boilerplate</title>
+		<script src="https://cdn.tailwindcss.com"></script>
 	</head>
 	<body>
 		@yield('body')
