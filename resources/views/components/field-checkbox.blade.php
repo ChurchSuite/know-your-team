fb5f5dd7606c9f8c43e313ebce4e39e06d5ad1e0
Luke Salter <luke@churchsuite.com>
2022-10-15 14:42:59 +0100

    Fixing x-model on checkbox

--- resources/views/components/field-checkbox.blade.php
+++ resources/views/components/field-checkbox.blade.php
@@ -8,7 +8,15 @@
 
 <div x-data class="relative flex items-start">
 	<div class="flex h-5 items-center">
-		<input id="{{ $id }}" aria-describedby="{{ $id }}-description" name="{{ $name }}" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" value={{ $value }}>
+		<input
+			id="{{ $id }}"
+			aria-describedby="{{ $id }}-description"
+			name="{{ $name }}"
+			type="checkbox"
+			class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+			value="{{ $value }}"
+			x-model="formData.{{ str_replace('[]', '', $name) }}"
+			>
 	</div>
 	<div class="ml-3 text-sm">
 		<x-field-label id="{{ $id }}">
