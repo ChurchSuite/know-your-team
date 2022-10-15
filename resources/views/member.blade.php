2ae7ae71d1b81f755c83b813536b6dd17da08dde
s-coates <shane@churchsuite.com>
2022-10-15 12:33:33 +0100

    update register and member view

--- resources/views/member.blade.php
+++ resources/views/member.blade.php
@@ -1,13 +1,21 @@
 @extends('layout')
 
 @section('content')
-	<x-form
-		action="/api/user"
-	>
-		<x-field-text label="First Name" name="first_name"/>
-		<x-field-text label="Last Name" name="last_name"/>
-		<x-field-email label="Email" name="email"/>
-		<x-field-text label="Job" name="job"/>
-		<x-button-submit>Submit</x-button-submit>
-	</x-form>
+	<x-page-section>
+		<x-page-title>
+			Add User
+		</x-page-title>
+
+		<x-form-wrapper>
+			<x-form
+				action="/api/user"
+			>
+				<x-field-text label="First Name" name="first_name"/>
+				<x-field-text label="Last Name" name="last_name"/>
+				<x-field-email label="Email" name="email"/>
+				<x-field-text label="Job" name="job"/>
+				<x-button-submit>Submit</x-button-submit>
+			</x-form>
+		</x-form-wrapper>
+	</x-page-section>
 @stop
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -13,10 +13,14 @@
 			<x-field-name firstnameName="first_name" lastnameName="last_name"/>
 			<x-field-text label="Email" name="email" type="email"/>
 			<x-field-text label="Job / Title" name="job"/>
-			<x-form-divider></x-form-divider>
-			{{-- Personality Type Checkboxes --}}
-			<x-button-post>Set it up!</x-button-post>
+			<x-form-divider>Personality Tests</x-form-divider>
+			<x-fieldset legend="Personality Tests">
+				<x-field-checkbox label="Enneagram" name="tests[enneagram]" suffix="stuff"/>
+				<x-field-checkbox label="Working Genius" name="tests[working_genius]" suffix="stuff"/>
+				<x-field-checkbox label="Myers Briggs" name="tests[myers_briggs]" suffix="stuff"/>
+			</x-fieldset>
+			<x-button-submit>Set it up!</x-button-submit>
 		</x-form>
 	</x-form-wrapper>
-</x-page-title>
+</x-page-section>
 @stop
