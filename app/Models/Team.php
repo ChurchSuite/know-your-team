893fca0b550f96d66b84348d4a1f21da68a8726f
s-coates <shane@churchsuite.com>
2022-10-15 09:00:07 +0100

    Team model

---
+++ app/Models/Team.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Team extends Model
+{
+	use HasFactory;
+
+	public $timestamps = true;
+
+	protected $fillable = [
+		'name',
+	];
+
+	protected $guarded = [
+		'id',
+		'organisation_id',
+	];
+
+}
