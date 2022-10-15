f935d953ac6af0fec2c67172a43327cd4e1c7da3
s-coates <shane@churchsuite.com>
2022-10-15 11:42:20 +0100

    add register form

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,5 +1,22 @@
 @extends('layout')
 
 @section('content')
-	Register
+<x-page-section>
+	<x-page-title>
+		Register your organisation
+	</x-page-title>
+
+	<x-form-wrapper>
+		<x-form action="/api/register">
+			<x-field-text label="Organisation" name="organisation"/>
+			<x-form-divider>About you</x-form-divider>
+			<x-field-name firstnameName="first_name" firstnameName="last_name"/>
+			<x-field-text label="Email" name="email" type="email"/>
+			<x-field-text label="Job / Title" name="job"/>
+			<x-form-divider></x-form-divider>
+			{{-- Personality Type Checkboxes --}}
+			<x-button-submit>Set it up!</x-button-submit>
+		</x-form>
+	</x-form-wrapper>
+</x-page-title>
 @stop
