ae9532395bb549e16bc320db3fb7cef6a8ace228
Ant Weedon <ant@churchsuite.com>
2022-10-15 17:01:14 +0100

    Add user profile picture update

--- resources/views/user/update.blade.php
+++ resources/views/user/update.blade.php
@@ -13,12 +13,15 @@
 					'last_name' => $user->last_name,
 					'email' => $user->email,
 					'job' => $user->job,
+					'profile_picture' => $user->profile_picture,
 				]) }}"
 				method="put"
 				>
+				<img class="mx-auto h-40 w-40 flex-shrink-0 rounded-full bg-gray-300" :src="formData.profile_picture" alt="">
 				<x-field-name label="Name" firstnameName="first_name" lastnameName="last_name"/>
 				<x-field-email label="Email" name="email"/>
 				<x-field-text label="Job" name="job"/>
+				<x-field-text label="Profile Picture" name="profile_picture"/>
 				<x-button-submit>Submit</x-button-submit>
 			</x-form>
 		</x-form-wrapper>
