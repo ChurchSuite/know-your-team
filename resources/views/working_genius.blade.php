6dc42a470c4204425dfe88957a769cd50a45cc39
Luke Salter <luke@churchsuite.com>
2022-10-15 16:49:45 +0100

    Adding test identifier to views

--- resources/views/working_genius.blade.php
+++ resources/views/working_genius.blade.php
@@ -1,6 +1,9 @@
 @extends('layout')
 
 @section('content')
+
+<div x-init="fetch('/api/data?test_identifier={{ $test_identifier }}').then(res => res.json()).then(data => console.log(data))"></div>
+
 <x-page-section>
 </x-page-section>
 @stop
