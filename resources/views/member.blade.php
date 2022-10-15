5ad9c7c3fabf9b4a0cd283fdc79af8a19f2fa688
s-coates <shane@churchsuite.com>
2022-10-15 12:34:49 +0100

    grouped name field

--- resources/views/member.blade.php
+++ resources/views/member.blade.php
@@ -10,8 +10,7 @@
 			<x-form
 				action="/api/user"
 			>
-				<x-field-text label="First Name" name="first_name"/>
-				<x-field-text label="Last Name" name="last_name"/>
+				<x-field-name label="Name" firstnameName="first_name" lastnameName="last_name"/>
 				<x-field-email label="Email" name="email"/>
 				<x-field-text label="Job" name="job"/>
 				<x-button-submit>Submit</x-button-submit>
