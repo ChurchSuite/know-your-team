9c9105c6ce1b0025b25d2d57ee421e46f070266a
Gavin Courtney <gavin@churchsuite.com>
2022-10-15 17:52:21 +0100

    Logo resize

--- resources/views/components/page-title.blade.php
+++ resources/views/components/page-title.blade.php
@@ -1,7 +1,7 @@
 @props(['subtitle' => ''])
 
 <div class="sm:mx-auto sm:w-full sm:max-w-md">
-	<img class="mx-auto h-12 w-auto" src="/logo.svg" alt="Know Your Team">
+	<img class="mx-auto h-32 w-auto" src="/logo.svg" alt="Know Your Team">
 	<h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">{{ $slot }}</h2>
 	<p class="mt-2 text-center text-sm text-gray-600">
 		{{ $subtitle }}
