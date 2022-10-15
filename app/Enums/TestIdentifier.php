63501d13d93de07141cf5d8dc16b7a6791d03e80
Ant Weedon <ant@churchsuite.com>
2022-10-15 11:35:10 +0100

    Added enum for TestIdentifier

---
+++ app/Enums/TestIdentifier.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Enums;
+
+enum TestIdentifier: string {
+    case MYERS_BRIGGS = 'myers_briggs';
+    case ENNEAGRAM = 'enneagram';
+    case WORKING_GENIUS = 'working_genius';
+}
